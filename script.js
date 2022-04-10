571110f98b53efe6ad3e8fc028ccce837a857618
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-04-09 21:09:57 -0400

    Create Borum Status webpage

---
+++ script.js
@@ -0,0 +1,19 @@
+const APP_STATUS = {
+    '0': 'Down',
+    '1': 'Operational',
+    'operational': 'Operational'
+}
+
+class AppStatus extends HTMLElement {
+    constructor() {
+        super();
+        const el = document.createElement('p');
+        el.innerText = this.getAttribute('name');
+        this.appendChild(el);
+        const status = document.createElement('p');
+        status.textContent = `${APP_STATUS[this.getAttribute('status')]}`;
+        this.appendChild(status);
+    }
+}
+
+customElements.define('app-status', AppStatus, { extends: 'article'} );
